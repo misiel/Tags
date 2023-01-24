5f60d477dec191d7cc172919a86dccbbd1ced121
Misiel <misiel.r9@gmail.com>
2023-01-24 14:35:10 -0500

    initial commit

---
+++ Tags/Tags/View/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tags
+//
+//  Created by Misiel on 1/24/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
